7ba6d2faccaf0a844e093bafdfb6ef7aa8a3ea29
Justin Lubin <jlubi333@gmail.com>
2015-11-27 01:29:31 -0500

    Finish basic user interface

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-test
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>jStitch</title>
+        <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700,Inconsolata' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="main.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="main.js"></script>
+    </head>
+    <body class="main-layout">
+        <nav class="bar">
+            <ul>
+                <li id="home" class="left"><a href="/">jStitch</a></li>
+                <li id="register" class="right"><a href="#register">Register</a></li>
+                <li id="log-in" class="right"><a href="#login">Log In</a></li>
+            </ul>
+        </nav>
+        <div class="main">
+            <p>You've crocheted</p>
+            <span id="current-stitch-count" class="count">33847</span>
+            <p>stitches!</p>
+        </div>
+        <footer class="bar">
+            <ul>
+                <li class="left">Project Stitch Count: <span id="project-stitch-count" class="count">638462</span></li>
+                <li class="right">Total Stitch Count: <span id="total-stitch-count" class="count">7638462</span></li>
+            </ul>
+        </footer>
+    </body>
+</html>
